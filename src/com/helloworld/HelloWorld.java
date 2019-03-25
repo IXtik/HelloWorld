a7ee4a0815c33d2e613fa50b28763c53b2ec9e7f
s.chugunov <ixti966@mail.ru>
2019-03-25 16:15:11 +0300

    Super new Hello World!

--- src/com/helloworld/HelloWorld.java
+++ src/com/helloworld/HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello GitHub!");
         System.out.println("12345");
+
+        Hello h = new Hello();
+        h.hello();
     }
 }
